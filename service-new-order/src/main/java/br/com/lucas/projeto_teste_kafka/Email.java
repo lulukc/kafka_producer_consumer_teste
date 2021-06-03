1cc7c223c4a18d9102da395f8b24f015f7e6acd2
Lucas Amancio <47485994+lulukc@users.noreply.github.com>
2021-06-03 18:23:21 -0300

    dividindo em modulos

---
+++ service-new-order/src/main/java/br/com/lucas/projeto_teste_kafka/Email.java
@@ -0,0 +1,11 @@
+package br.com.lucas.projeto_teste_kafka;
+
+public class Email {
+
+    private String subject,body;
+
+    public Email(String subject, String body) {
+        this.subject = subject;
+        this.body = body;
+    }
+}
